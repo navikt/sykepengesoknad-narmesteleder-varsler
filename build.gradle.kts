668d079f0f014b0b1904b57d5dbd1049a14d6541
golshidbahadorian <golshid.bahadorian@nav.no>
2021-05-28 13:35:46 +0200

    lint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.5"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.0"
-	kotlin("plugin.spring") version "1.5.0"
+    id("org.springframework.boot") version "2.4.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.0"
+    kotlin("plugin.spring") version "1.5.0"
 }
 
 group = "no.nav.helse.flex"
@@ -13,12 +13,12 @@ description = "sykepengesoknad-narmesteleder-varsler"
 java.sourceCompatibility = JavaVersion.VERSION_16
 
 buildscript {
-	repositories {
-		maven("https://plugins.gradle.org/m2/")
-	}
-	dependencies {
-		classpath("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
-	}
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
+    }
 }
 
 ext["nimbus-jose-jwt.version"] = "8.20" // https://nav-it.slack.com/archives/C01381BAT62/p1611056940004800
@@ -30,21 +30,21 @@ val githubUser: String by project
 val githubPassword: String by project
 
 repositories {
-	mavenCentral()
-	maven {
-		url = uri("https://maven.pkg.github.com/navikt/maven-release")
-	}
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/maven-release")
+    }
 
-	maven {
-		url = uri("https://maven.pkg.github.com/navikt/freg-security")
-	}
-	maven {
-		url = uri("https://maven.pkg.github.com/navikt/syfosm-common")
-		credentials {
-			username = githubUser
-			password = githubPassword
-		}
-	}
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/freg-security")
+    }
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/syfosm-common")
+        credentials {
+            username = githubUser
+            password = githubPassword
+        }
+    }
 }
 
 val testContainersVersion = "1.15.3"
@@ -54,45 +54,45 @@ val kluentVersion = "1.65"
 val syfoKafkaVersion = "2021.04.08-13.45-0da5bd8e"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.kafka:spring-kafka")
-	implementation("no.nav.security:token-client-spring:$tokenSupportVersion")
-	implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
-	implementation("org.slf4j:slf4j-api")
-	implementation("org.springframework.boot:spring-boot-starter-logging")
-	implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("no.nav.syfo.kafka:felles:$syfoKafkaVersion")
-	implementation("org.aspectj:aspectjrt")
-	implementation("org.aspectj:aspectjweaver")
-	implementation("org.hibernate.validator:hibernate-validator")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("io.micrometer:micrometer-registry-prometheus")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("no.nav.security:token-client-spring:$tokenSupportVersion")
+    implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
+    implementation("org.slf4j:slf4j-api")
+    implementation("org.springframework.boot:spring-boot-starter-logging")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("no.nav.syfo.kafka:felles:$syfoKafkaVersion")
+    implementation("org.aspectj:aspectjrt")
+    implementation("org.aspectj:aspectjweaver")
+    implementation("org.hibernate.validator:hibernate-validator")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.testcontainers:kafka:$testContainersVersion")
-	testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
-	testImplementation("org.awaitility:awaitility")
-	testImplementation("org.hamcrest:hamcrest-library")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.testcontainers:kafka:$testContainersVersion")
+    testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
+    testImplementation("org.awaitility:awaitility")
+    testImplementation("org.hamcrest:hamcrest-library")
 }
 
 tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
-	this.archiveFileName.set("app.jar")
+    this.archiveFileName.set("app.jar")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "16"
-		kotlinOptions.allWarningsAsErrors = true
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "16"
+        kotlinOptions.allWarningsAsErrors = true
+    }
 }
 tasks.withType<Test> {
-	useJUnitPlatform()
-	testLogging {
-		events("PASSED", "FAILED", "SKIPPED")
-	}
+    useJUnitPlatform()
+    testLogging {
+        events("PASSED", "FAILED", "SKIPPED")
+    }
 }
