469ad2c3e990b7252653d70ff1d81e2a29182b3a
golshidbahadorian <golshid.bahadorian@nav.no>
2021-05-28 12:51:34 +0200

    Legger til nødvendige dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,75 +1,98 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
-
-val ktlint by configurations.creating
-
-val outputDir = "${project.buildDir}/reports/ktlint/"
-val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
 
 plugins {
 	id("org.springframework.boot") version "2.4.5"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id("com.github.johnrengelman.shadow") version "6.1.0"
-	kotlin("jvm") version "1.4.32"
-	kotlin("plugin.spring") version "1.4.32"
+	kotlin("jvm") version "1.5.0"
+	kotlin("plugin.spring") version "1.5.0"
 }
 
 group = "no.nav.helse.flex"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+version = "1.0.0"
+description = "sykepengesoknad-narmesteleder-varsler"
+java.sourceCompatibility = JavaVersion.VERSION_16
+
+buildscript {
+	repositories {
+		maven("https://plugins.gradle.org/m2/")
+	}
+	dependencies {
+		classpath("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
+	}
+}
+
+ext["nimbus-jose-jwt.version"] = "8.20" // https://nav-it.slack.com/archives/C01381BAT62/p1611056940004800
+ext["okhttp3.version"] = "4.9.0" // For at token support testen kjører (tror jeg)
+
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+val githubUser: String by project
+val githubPassword: String by project
 
 repositories {
 	mavenCentral()
+	maven {
+		url = uri("https://maven.pkg.github.com/navikt/maven-release")
+	}
+
+	maven {
+		url = uri("https://maven.pkg.github.com/navikt/freg-security")
+	}
+	maven {
+		url = uri("https://maven.pkg.github.com/navikt/syfosm-common")
+		credentials {
+			username = githubUser
+			password = githubPassword
+		}
+	}
 }
 
-dependencies {
-	ktlint("com.pinterest:ktlint:0.40.0")
+val testContainersVersion = "1.15.3"
+val tokenSupportVersion = "1.3.7"
+val logstashLogbackEncoderVersion = "6.6"
+val kluentVersion = "1.65"
+val syfoKafkaVersion = "2021.04.08-13.45-0da5bd8e"
 
+dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.kafka:spring-kafka")
+	implementation("no.nav.security:token-client-spring:$tokenSupportVersion")
+	implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
+	implementation("org.slf4j:slf4j-api")
+	implementation("org.springframework.boot:spring-boot-starter-logging")
+	implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("no.nav.syfo.kafka:felles:$syfoKafkaVersion")
+	implementation("org.aspectj:aspectjrt")
+	implementation("org.aspectj:aspectjweaver")
+	implementation("org.hibernate.validator:hibernate-validator")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("io.micrometer:micrometer-registry-prometheus")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.testcontainers:kafka:$testContainersVersion")
+	testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
+	testImplementation("org.awaitility:awaitility")
+	testImplementation("org.hamcrest:hamcrest-library")
 }
 
-val ktlintCheck by tasks.creating(JavaExec::class) {
-	inputs.files(inputFiles)
-	outputs.dir(outputDir)
-
-	description = "Check Kotlin code style."
-	classpath = ktlint
-	main = "com.pinterest.ktlint.Main"
-	args = listOf("src/**/*.kt")
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+	this.archiveFileName.set("app.jar")
 }
 
-val ktlintFormat by tasks.creating(JavaExec::class) {
-	inputs.files(inputFiles)
-	outputs.dir(outputDir)
-
-	description = "Fix Kotlin code style deviations."
-	classpath = ktlint
-	main = "com.pinterest.ktlint.Main"
-	args = listOf("-F", "src/**/*.kt")
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "16"
+		kotlinOptions.allWarningsAsErrors = true
+	}
 }
-
-tasks {
-
-	withType<ShadowJar> {
-		transform(ServiceFileTransformer::class.java) {
-			setPath("META-INF/cxf")
-			include("bus-extensions.txt")
-		}
+tasks.withType<Test> {
+	useJUnitPlatform()
+	testLogging {
+		events("PASSED", "FAILED", "SKIPPED")
 	}
-
-	withType<KotlinCompile> {
-		kotlinOptions {
-			freeCompilerArgs = listOf("-Xjsr305=strict")
-			jvmTarget = "11"
-		}
-	}
-
-	withType<Test> {
-		useJUnitPlatform()
-	}
-
 }
