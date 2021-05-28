c6c10caecbbe7c20e3e43d6d3d3d48c4a84e1f32
golshidbahadorian <golshid.bahadorian@nav.no>
2021-05-28 13:15:34 +0200

    Appen skal kjøre i gcp istedenfor fss java-versjon til 16, upload-artifact@v2.2.3, cache@v2.1.6

--- buildlatest.sh
+++ buildlatest.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-echo "Bygger sykepengesoknad-narmesteleder-varsler for bruk i flex-docker-compose"
-./gradlew ktlintFormat
-./gradlew shadowJar
-docker build -t sykepengesoknad-narmesteleder-varsler:latest .
+echo "Bygger sykepengesoknad-narmesteleder-varsler latest"
+
+./gradlew bootJar
+
+docker build . -t sykepengesoknad-narmesteleder-varsler:latest
