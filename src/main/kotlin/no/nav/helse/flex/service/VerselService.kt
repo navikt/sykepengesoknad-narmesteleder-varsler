4f79820e23ca0d006104d87329059d7eba6858ef
golshidbahadorian <golshid.bahadorian@nav.no>
2021-06-01 17:51:15 +0200

    Kaller syfoservicestrangler for å opprette oppgave

---
+++ src/main/kotlin/no/nav/helse/flex/service/VerselService.kt
@@ -0,0 +1,33 @@
+package no.nav.helse.flex.service
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.helse.flex.client.OpprettHendelseRequest
+import no.nav.helse.flex.client.SyfoservicestranglerClient
+import no.nav.helse.flex.logger
+import no.nav.helse.flex.objectMapper
+import no.nav.syfo.kafka.felles.SykepengesoknadDTO
+import org.springframework.stereotype.Service
+
+@Service
+class VerselService(
+    val syfoservicestranglerClient: SyfoservicestranglerClient
+) {
+
+    val log = logger()
+
+    fun opprettVarsel(soknadString: String) {
+        val soknad = soknadString.tilSykepengesoknadDTO()
+        log.debug("Mottok soknad ${soknad.id}")
+
+        syfoservicestranglerClient.opprettOppgave(
+            OpprettHendelseRequest(
+                soknadId = soknad.id,
+                aktorId = null, // TODO: trengs det?
+                orgnummer = null, // TODO: trengs det?
+                type = soknad.type.name // TODO: samme som søknadstype eller meldingstype?
+            )
+        )
+    }
+
+    fun String.tilSykepengesoknadDTO(): SykepengesoknadDTO = objectMapper.readValue(this)
+}
