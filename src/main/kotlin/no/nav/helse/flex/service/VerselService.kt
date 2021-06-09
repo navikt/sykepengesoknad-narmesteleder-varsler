24a6dc67e0b2edd18e5a4e20bf03f73f061afd10
golshidbahadorian <golshid.bahadorian@nav.no>
2021-06-09 12:11:51 +0200

    Oppretter oppgave hvis personen er arbeidstaker og søknaden er sendt

--- src/main/kotlin/no/nav/helse/flex/service/VerselService.kt
+++ src/main/kotlin/no/nav/helse/flex/service/VerselService.kt
@@ -5,6 +5,9 @@ import no.nav.helse.flex.client.OpprettHendelseRequest
 import no.nav.helse.flex.client.SyfoservicestranglerClient
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.objectMapper
+import no.nav.syfo.kafka.felles.ArbeidssituasjonDTO
+import no.nav.syfo.kafka.felles.SoknadsstatusDTO
+import no.nav.syfo.kafka.felles.SoknadstypeDTO
 import no.nav.syfo.kafka.felles.SykepengesoknadDTO
 import org.springframework.stereotype.Service
 
@@ -22,14 +25,19 @@ class VerselService(
 
         val aktorId = identService.hentAktorIdForFnr(soknad.fnr)
 
-        syfoservicestranglerClient.opprettOppgave(
-            OpprettHendelseRequest(
-                soknadId = soknad.id,
-                aktorId = aktorId,
-                orgnummer = soknad.arbeidsgiver?.orgnummer,
-                type = soknad.type.name
+        if ((SoknadstypeDTO.ARBEIDSTAKERE == soknad.type ||
+                (SoknadstypeDTO.BEHANDLINGSDAGER == soknad.type && soknad.arbeidssituasjon == ArbeidssituasjonDTO.ARBEIDSTAKER))
+            && SoknadsstatusDTO.SENDT == soknad.status
+            && soknad.sendtArbeidsgiver != null) {
+            syfoservicestranglerClient.opprettOppgave(
+                OpprettHendelseRequest(
+                    soknadId = soknad.id,
+                    aktorId = aktorId,
+                    orgnummer = soknad.arbeidsgiver?.orgnummer,
+                    type = soknad.type.name
+                )
             )
-        )
+        }
     }
 
     fun String.tilSykepengesoknadDTO(): SykepengesoknadDTO = objectMapper.readValue(this)
