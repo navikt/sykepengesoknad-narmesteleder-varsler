72ad5967452ad57f3970416cfed296dce3124c45
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-09-02 08:26:50 +0200

    Litt renaming og flytting

--- src/main/kotlin/no/nav/helse/flex/kafka/SykepengesoknadListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/SykepengesoknadListener.kt
@@ -3,16 +3,13 @@ package no.nav.helse.flex.kafka
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.service.BrukerOppgaveService
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.springframework.context.event.EventListener
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.event.ConsumerStoppedEvent
-import org.springframework.kafka.listener.KafkaMessageListenerContainer
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 
 @Component
 class SykepengesoknadListener(
-    private val varselService: BrukerOppgaveService
+    private val brukerOppgaveService: BrukerOppgaveService
 ) {
 
     private val log = logger()
@@ -20,29 +17,11 @@ class SykepengesoknadListener(
     @KafkaListener(topics = [FLEX_SYKEPENGESOKNAD_TOPIC])
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
         try {
-            varselService.opprettVarsel(cr.value())
+            brukerOppgaveService.opprettBrukeroppgave(cr.value())
             acknowledgment.acknowledge()
         } catch (e: Exception) {
             log.error("Feil ved mottak av record med key: ${cr.key()} offset: ${cr.offset()} partition: ${cr.partition()}", e)
             throw e
-        }
-    }
-
-    @EventListener
-    fun eventHandler(event: ConsumerStoppedEvent) {
-        if (event.reason == ConsumerStoppedEvent.Reason.NORMAL) {
-            log.debug("Consumer stoppet grunnet NORMAL")
-            return
-        }
-        log.error("Consumer stoppet grunnet ${event.reason}")
-        if (event.source is KafkaMessageListenerContainer<*, *> &&
-            event.reason == ConsumerStoppedEvent.Reason.AUTH
-        ) {
-            val container = event.source as KafkaMessageListenerContainer<*, *>
-            if (container.groupId == "sykepengesoknad-narmesteleder-varsler") {
-                log.info("Trying to restart consumer, creds may be rotated")
-                container.start()
-            }
         }
     }
 }
