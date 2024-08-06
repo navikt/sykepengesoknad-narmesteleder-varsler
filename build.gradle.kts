050cd5cc97d522294751963bc77dd5a88eb1d13d
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2024-08-06 10:38:52 +0200

    Bump til Spring Boot 3.3.1 (#324)
    org.springframework.boot from 3.2.5 to 3.3.1
    tokenSupportVersion from 4.1.7 to 5.0.1
    io.spring.dependency-management from 1.1.5 to 1.1.6
    Byttet fra flyway-core til flyway-database-postgresql

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("org.springframework.boot") version "3.2.5"
+    id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.6"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
     kotlin("jvm") version "1.9.24"
@@ -31,7 +31,7 @@ repositories {
 }
 
 val testContainersVersion = "1.20.0"
-val tokenSupportVersion = "4.1.7"
+val tokenSupportVersion = "5.0.1"
 val logstashLogbackEncoderVersion = "7.4"
 val kluentVersion = "1.73"
 val sykepengesoknadKafkaVersion = "2024.05.04-08.31-672172ee"
@@ -49,7 +49,7 @@ dependencies {
     implementation("org.hibernate.validator:hibernate-validator")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("org.postgresql:postgresql")
-    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
     implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
