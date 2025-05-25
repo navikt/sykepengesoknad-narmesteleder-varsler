2416f17485e696e6bde3f72c8721095d13ce63a3
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2025-05-25 13:45:22 +0200

    Bruk apache/kafka-native:3.9.1 i FellesTestOppsett (#433)

--- src/test/kotlin/no/nav/helse/flex/FellesTestOppsett.kt
+++ src/test/kotlin/no/nav/helse/flex/FellesTestOppsett.kt
@@ -68,7 +68,7 @@ abstract class FellesTestOppsett {
                 System.setProperty("spring.datasource.password", it.password)
             }
 
-            KafkaContainer(DockerImageName.parse("apache/kafka-native:4.0.0")).also {
+            KafkaContainer(DockerImageName.parse("apache/kafka-native:3.9.1")).also {
                 it.start()
                 System.setProperty("KAFKA_BROKERS", it.bootstrapServers)
             }
