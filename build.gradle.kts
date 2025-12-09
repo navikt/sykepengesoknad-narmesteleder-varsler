639a692d6e4f05a219f3eb16edc37185beb06018
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2025-12-09 11:30:35 +0100

    Bump Testcontainers til 2.0.2 (#490)

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ repositories {
     }
 }
 
-val testContainersVersion = "1.21.3"
+val testContainersVersion = "2.0.2"
 val tokenSupportVersion = "5.0.39"
 val logstashLogbackEncoderVersion = "8.1"
 val kluentVersion = "1.73"
@@ -58,10 +58,10 @@ dependencies {
     implementation("io.confluent:kafka-avro-serializer:$confluentVersion")
     implementation("no.nav.teamdokumenthandtering:teamdokumenthandtering-avro-schemas:$doknotifikasjonAvroVersion")
 
-    testImplementation(platform("org.testcontainers:testcontainers-bom:$testContainersVersion"))
+    testImplementation("org.testcontainers:testcontainers:$testContainersVersion")
+    testImplementation("org.testcontainers:testcontainers-postgresql:$testContainersVersion")
+    testImplementation("org.testcontainers:testcontainers-kafka:$testContainersVersion")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.testcontainers:postgresql")
-    testImplementation("org.testcontainers:kafka")
     testImplementation("org.awaitility:awaitility")
     testImplementation("no.nav.security:token-validation-spring-test:$tokenSupportVersion")
     testImplementation("org.amshove.kluent:kluent:$kluentVersion")
